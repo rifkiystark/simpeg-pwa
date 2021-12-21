388113a9ef03ce1695a0168339d528d795f7fecd
rifkiystark <ananda.rifkiy32@gmail.com>
2021-12-21 11:23:31 +0700

    fix pagination algorithm

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -5,8 +5,8 @@ import * as XLSX from 'xlsx';
 function Table({ data, tableName }) {
     let [dataTable, setDataTable] = useState(data)
     let [activePage, setActivePage] = useState(1)
-    
-    let totalDataPerPage = 10
+
+    let totalDataPerPage = 1
     let [totalPage, setTotalPage] = useState(parseInt(dataTable.data.length / totalDataPerPage) + (dataTable.data.length % totalDataPerPage > 0 ? 1 : 0))
 
     const search = (e) => {
@@ -40,7 +40,7 @@ function Table({ data, tableName }) {
         const ws = XLSX.utils.json_to_sheet(data.data);
         const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
         const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
-        const dataExcel = new Blob([excelBuffer], {type: fileType});
+        const dataExcel = new Blob([excelBuffer], { type: fileType });
         FileSaver.saveAs(dataExcel, tableName + fileExtension);
     }
     return (<>
@@ -86,9 +86,9 @@ function Table({ data, tableName }) {
                 </div>
             </div>
         </div>
-        <div class="card-footer d-flex align-items-center">
-            <p class="m-0 text-muted">Showing <span>{(activePage - 1) * totalDataPerPage + 1}</span> to <span>{(activePage * totalDataPerPage < dataTable.data.length) ? activePage * totalDataPerPage : dataTable.data.length}</span> of <span>{dataTable.data.length}</span> entries</p>
-            <ul class="pagination m-0 ms-auto">
+        <div class="card-footer row">
+            <p class="text-muted col-md-6 col-sm-12">Showing <span>{(activePage - 1) * totalDataPerPage + 1}</span> to <span>{(activePage * totalDataPerPage < dataTable.data.length) ? activePage * totalDataPerPage : dataTable.data.length}</span> of <span>{dataTable.data.length}</span> entries</p>
+            <ul class="pagination col-md-6 col-sm-12 justify-content-md-end justify-content-center">
                 <li class={`page-item ` + (activePage === 1 ? 'disabled' : '')}>
                     <button className="page-link" tabIndex="-1" onClick={() => {
                         if (activePage > 1) {
@@ -96,7 +96,7 @@ function Table({ data, tableName }) {
                         }
                     }}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="15 6 9 12 15 18"></polyline></svg>
-                        prev
+
                     </button>
                 </li>
                 {
@@ -107,7 +107,7 @@ function Table({ data, tableName }) {
                             return <li className="page-item active"><button className="page-link">{i + 1}</button></li>
                         } else if ((i + 1) - activePage === 4) {
                             return <li className="page-item"><button className="page-link">..</button></li>
-                        } else if (activePage - (i + 1) < 4 || (i + 1) - activePage < 4) {
+                        } else if ((activePage - i < 4 && i < activePage) || (i - activePage < 4 && i > activePage)) {
                             return <li className="page-item" onClick={() => {
                                 setActivePage(i + 1)
                             }}><button className="page-link">{i + 1}</button></li>
@@ -118,13 +118,13 @@ function Table({ data, tableName }) {
                     })
                 }
 
+
                 <li className={`page-item ` + (activePage === totalPage ? 'disabled' : '')}>
                     <button className="page-link" onClick={() => {
                         if (activePage < totalPage) {
                             setActivePage(activePage + 1)
                         }
                     }}>
-                        next
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="9 6 15 12 9 18"></polyline></svg>
                     </button>
                 </li>
